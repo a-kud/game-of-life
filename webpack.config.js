81b00447a911450948711ef5473b849917ca3f47
Andrew Kud <kud@mm.st>
2017-07-30 05:06:07 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var config = {
+    context: path.join(__dirname, "src"),
+    entry: [
+        "./main.js",
+        "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000",
+    ],
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "bundle.js",
+        publicPath: "/build/",
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.sass$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, "node_modules"),
+        ],
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ]
+};
+
+module.exports = config;
