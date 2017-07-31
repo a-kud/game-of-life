e558759e446a6567b21c0fa732056493674e382b
Andrew Kud <kud@mm.st>
2017-07-31 04:29:26 +0300

    Manage styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React, {Component} from "react";
+import Controls from "./Controls";
 
 class App extends Component {
-
+    render() {
+        return(
+            <Controls />
+        );
+    }
 }
 
 export default App;
