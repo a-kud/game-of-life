aa10399b4ba2ac17500164a157f5ef4de168e873
Andrew Kud <kud@mm.st>
2017-07-31 04:27:59 +0300

    Add control buttons

---
+++ src/components/Controls.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Controls extends Component {
+
+    render() {
+        return(
+            <div class="btns-control">
+                <button>Start</button>
+                <button>Pause</button>
+                <button>Reset</button>
+            </div>
+        );
+    }
+}
+
+export default Controls;
