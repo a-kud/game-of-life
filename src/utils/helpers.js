2eabc03058c7e0022f9f0ce6a3a9de3c52ee0366
Andrew Kud <kud@mm.st>
2017-08-17 20:12:34 +0300

    Add helper method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,13 +7,24 @@ export function getColor(ctx, sx, sy) {
     return Color(ctx.getImageData(sx,sy,1,1).data).rgbNumber();
 }
 
+/*sx, sy - coordinates of mouse click*/
 export function getNeighborsCoordinates(sx, sy) {
 
     const DIAGONAL = 10*Math.sqrt(2); //cell diagonal length
-    let leftTopCoord = [sx-(sx%10), sy-(sy%10)];
+    let xTopLeft = sx-(sx%10); // 10 - cell length
+    let yTopLeft = sy-(sy%10);
 
-    function getDiagCoord(sx, sy) {
-        return [[sx-10, sy-10]]
+    /*x, y - coordinates of top left vertex of cell*/
+    function getDiagCoord(x, y) {
+        // starting to calculate vertices coordinates:
+        let firstNeighborX = x - 10;
+        let firstNeighborY = y + 10;
+
+        return [
+                [firstNeighborX, firstNeighborY],
+                [firstNeighborX + 10, firstNeighborY],
+                [firstNeighborX + 20, firstNeighborY],    
+               ]
     }
 
 
