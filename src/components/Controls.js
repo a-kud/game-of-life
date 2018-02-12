3eab34b56bdee27d2be63e72bfac75602001eea9
Andrew Kud <kud@mm.st>
2018-02-12 22:21:57 +0200

    Add material-ui

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Button from 'material-ui/Button'
 
 class Controls extends Component {
   static propTypes = {
-    onClick: PropTypes.func
+    onClick: PropTypes.func.isRequired
   }
 
   render () {
     return (
       <div className='btns-control'>
-        <button onClick={this.props.handleStart}>Start</button>
-        <button onClick={this.props.handleManualStart}>Step-by-step</button>
-        <button onClick={this.props.handleReset}>Reset</button>
+        <Button variant='raised' onClick={this.props.handleStart}>
+          Start
+        </Button>
+        <Button variant='raised' onClick={this.props.handleManualStart}>
+          Step-by-step
+        </Button>
+        <Button variant='raised' onClick={this.props.handleReset}>
+          Reset
+        </Button>
         <span id='generation'>Generation: {this.props.generation}</span>
       </div>
     )
